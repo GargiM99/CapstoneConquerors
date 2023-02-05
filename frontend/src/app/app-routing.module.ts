5e770b40f27b941f2c7e563fcfb92e01a253a6a3
HamzaAtcha <atchah@sheridancollege.ca>
2023-02-05 11:01:32 -0500

    Changes of Frontend Demo

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,8 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { AgentComponentsModule } from './agent-components/agent-components.module';
 import { AgentFormsComponent } from './agent-components/agent-forms/agent-forms.component';
 import { AgentListComponent } from './agent-components/agent-list/agent-list.component';
-import { UserFormComponent } from './components/user-form/user-form.component';
-import { UserListComponent } from './components/user-list/user-list.component';
 
 const routes: Routes = [
   { path : 'agent', component: AgentListComponent},
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ComponentsModule } from './components/components.module';
 import { AgentComponentsModule } from './agent-components/agent-components.module';
 
 @NgModule({
@@ -13,7 +12,6 @@ import { AgentComponentsModule } from './agent-components/agent-components.modul
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ComponentsModule,
     AgentComponentsModule
   ],
   providers: [],
