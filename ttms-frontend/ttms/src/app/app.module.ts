8799886859aab32784764b6aa7b0ddc6e775c2aa
HamzaAtcha <atchah@sheridancollege.ca>
2023-03-02 20:42:27 -0500

    Created the header
    Installed bootstrap properly and created the nav bar/header.

--- ttms-frontend/ttms/src/app/app.module.ts
+++ ttms-frontend/ttms/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TtmsCommonModule } from './ttms-common/ttms-common.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TtmsCommonModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ ttms-frontend/ttms/src/app/ttms-common/ttms-header/ttms-header.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-ttms-header',
+  templateUrl: './ttms-header.component.html',
+  styleUrls: ['./ttms-header.component.scss']
+})
+export class TtmsHeaderComponent implements OnInit {
+
+  ngOnInit(): void {
+  }
+
+}
