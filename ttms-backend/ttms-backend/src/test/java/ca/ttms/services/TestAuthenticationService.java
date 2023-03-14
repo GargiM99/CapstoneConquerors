2e2bce34ab909c11692cff93218b3541cb2ebfd5
HamzaAtcha <atchah@sheridancollege.ca>
2023-03-13 21:52:39 -0400

    Finish Authentication Tests
    Finish writing all the authentication tests, including register and auth.

--- ttms-backend/ttms-backend/src/test/java/ca/ttms/services/TestAuthenticationService.java
+++ ttms-backend/ttms-backend/src/test/java/ca/ttms/services/TestAuthenticationService.java
@@ -1,7 +1,10 @@
 package ca.ttms.services;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +12,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import ca.ttms.beans.ResponseToken;
+import ca.ttms.beans.User;
+import ca.ttms.beans.UserAuthenticationDetails;
 import ca.ttms.beans.UserRegisterDetails;
 import ca.ttms.repositories.TokenRepo;
 import ca.ttms.repositories.UserRepo;
@@ -32,7 +37,7 @@ class TestAuthenticationService {
 	private AuthenticationService service;
 
 	@Test
-	//Description: Register a user and checks that there is a token
+	//Register a user and checks that there is a token
 	void testPassRegisterUser1() {
 		
 		UserRegisterDetails inputUser = UserRegisterDetails.builder()
@@ -50,7 +55,7 @@ class TestAuthenticationService {
 	}
 	
 	@Test
-	//Description: Register a user and checks that there is a token and user
+	//Register a user and checks that there is a token and user
 	void testPassRegisterUser2() {
 		
 		UserRegisterDetails inputUser = UserRegisterDetails.builder()
@@ -73,13 +78,76 @@ class TestAuthenticationService {
 	}
 	
 	@Test
-	//Description: Register a user and checks that there is a token and user
+	//Register a null user and checks if it returns null
 	void testFailRegisterUser() {
 		
 		UserRegisterDetails inputUser = null;
+		ResponseToken expectedToken = null;
 		
-		ResponseToken expectedToken = service.registerUser(inputUser);
-		
+		ResponseToken resultToken = service.registerUser(inputUser);
+		assertEquals(expectedToken, resultToken);
 	}
 
+	
+	@Test
+	//Authenticate a user and checks that there is a token and it's valid
+	void testPassAuthenticateUser1() {
+		
+		UserRegisterDetails inputUser = UserRegisterDetails.builder()
+				.firstname("Gargi")
+				.lastname("Matoo")
+				.email("gm@gmail.com")
+				.username("gm")
+				.password("3456")
+				.build();
+		
+		ResponseToken inputToken = service.registerUser(inputUser);
+		
+		UserAuthenticationDetails inputAuth = UserAuthenticationDetails.builder()
+				.username(inputUser.getUsername())
+				.password(inputUser.getPassword())
+				.build();
+	
+		ResponseToken resultToken = service.authenticateUser(inputAuth);
+		var respnoseUser = userRepo.findByUsername(inputUser.getUsername());
+		var resultUser = respnoseUser.orElse(null);
+		
+		var respnoseToken = tokenRepo.findAllValidTokenByUser(resultUser.getId());
+		var expectedToken = respnoseToken.get(1);
+		
+		assertEquals(resultToken.getToken(),expectedToken.getToken());
+	}
+	
+	@Test
+	//Authenticate a user and checks if token is valid
+	void testPassAuthenticateUser2() {
+		
+		UserRegisterDetails inputUser = UserRegisterDetails.builder()
+				.firstname("Zunaira")
+				.lastname("Malik")
+				.email("zm@gmail.com")
+				.username("zm")
+				.password("4567")
+				.build();
+		
+		ResponseToken inputToken = service.registerUser(inputUser);
+		
+		UserAuthenticationDetails inputAuth = UserAuthenticationDetails.builder()
+				.username(inputUser.getUsername())
+				.password(inputUser.getPassword())
+				.build();
+	
+		ResponseToken resultToken = service.authenticateUser(inputAuth);
+	
+		
+		assertTrue(resultToken.getToken().length() > 150, "Token not recieved");
+	}
+	
+	//Tests no existant user
+    @Test
+    void testFailAuthenticateUser() {
+      Optional<User> foundUser = userRepo.findByUsername("nonexistent");
+
+      assertFalse(foundUser.isPresent());
+    }
 }
