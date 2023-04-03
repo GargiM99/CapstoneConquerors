9694cf8ae1d39568864b40962b79fb7b0702a597
z2701 <66741508+z2701@users.noreply.github.com>
2023-04-03 17:59:45 -0400

    Commit
    this is second commit

--- viewDescription/src/app/viewdescription/viewdescription.component.ts
+++ viewDescription/src/app/viewdescription/viewdescription.component.ts
@@ -6,7 +6,22 @@ import { Component, ElementRef, HostListener, Input, Renderer2, TemplateRef, Vie
   styleUrls: ['./viewdescription.component.css']
 })
 export class ViewdescriptionComponent {
-
+  adults: number = 0;
+  children: number = 0;
+  days: number = 0;
+  FulldaysPerService: number = 0;
+  QuickdaysPerService: number = 0;
+  snacksPreference: boolean = true;
+  mealPrice: number = 0;
+  snackPrice: number=0;
+  totalPrice: number=0;
+  totalPricePerDay: number=0;
+  adultsError: string = '';
+  childrenError: string = '';
+  daysError: string = '';
+  FulldaysPerServiceError: string = '';
+  QuickdaysPerServiceError: string = '';
+  daysPerServiceError : string = '';
   /*constructor(private elRef:ElementRef,
               private rendered:Renderer2,
               private viewRefContainer:ViewContainerRef){}*/
